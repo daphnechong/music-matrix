d47cc48d665f9e51eb91a9c7f4a6750467df360d
Daphne Chong <daphne.h.chong@gmail.com>
2014-06-16 11:28:38 +1000

    initial commit of basic project files

---
+++ app.js
@@ -0,0 +1,33 @@
+var express    = require('express'); 		// call express
+var app        = express(); 				// define our app using express
+var bodyParser = require('body-parser');
+
+// configure app to use bodyParser()
+// this will let us get the data from a POST
+app.use(bodyParser());
+app.use(express.static(__dirname + '/public'));
+
+var port = process.env.PORT || 3000; 		// set our port
+
+// ROUTES FOR OUR API
+// =============================================================================
+var router = express.Router(); 				// get an instance of the express Router
+
+// middleware to use for all requests
+router.use(function(req, res, next) {
+	// do logging
+	console.log('Something is happening.');
+	next(); // make sure we go to the next routes and don't stop here
+});
+
+// test route to make sure everything is working (accessed at GET http://localhost:8080/api)
+router.get('/', function(req, res) {
+	res.sendfile(__dirname + '/public/views/index.html');	
+});
+
+app.use('/', router);
+
+// START THE SERVER
+// =============================================================================
+app.listen(port);
+console.log('Magic happens on port ' + port);
